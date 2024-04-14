21d9ddf566c55a704d3fe3567b405be03ff12905
TONGQIA <1227894812@qq.com>
2024-04-14 20:13:21 +0800

    init:初始化

---
+++ src/main/java/com/tong/gitcommandtest/GitCommandTestApplication.java
@@ -0,0 +1,13 @@
+package com.tong.gitcommandtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCommandTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitCommandTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tong/gitcommandtest/GitCommandTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tong.gitcommandtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCommandTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
